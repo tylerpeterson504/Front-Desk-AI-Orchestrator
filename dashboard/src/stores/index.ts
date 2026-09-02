9b7fc0854e8898ed828a9b2d43194bd5cd3c6d8e
tylerpeterson504 <tyler.peterson504@gmail.com>
2026-09-02 09:09:22 -0500

    feat(dashboard): add stores index file for centralized exports

---
+++ dashboard/src/stores/index.ts
@@ -0,0 +1,5 @@
+export { useAuthStore, tokenStore } from './authStore';
+export { usePropertiesStore } from './propertiesStore';
+export { useTemplatesStore } from './templatesStore';
+export { useShiftNotesStore } from './shiftNotesStore';
+export { useAuditLogsStore } from './auditLogsStore';
