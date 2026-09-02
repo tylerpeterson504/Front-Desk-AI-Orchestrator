4f68dedd203abd09acff7419eb025ede4f31c464
tylerpeterson504 <tyler.peterson504@gmail.com>
2026-09-02 09:26:19 -0500

    feat(extension): add utils index file

---
+++ extension/src/utils/index.ts
@@ -0,0 +1,19 @@
+export {
+  sendToSidePanel,
+  broadcastToSidePanel,
+  getCurrentProperty,
+  getApiUrl,
+  safeTextContent,
+  safeGetAttribute,
+  debounce,
+  throttle,
+  retryWithBackoff,
+  isVisible,
+  waitForElement,
+  waitForVisible,
+  extractGuestInfo,
+  extractChatContext,
+  injectText,
+  injectTextToSelector,
+  logError
+} from './contentUtils';
