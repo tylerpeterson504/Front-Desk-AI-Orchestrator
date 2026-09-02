755b810c903d02102c5ebc77b0a8d4f950966156
tylerpeterson504 <tyler.peterson504@gmail.com>
2026-09-02 09:12:38 -0500

    feat(dashboard): add components index file for centralized exports

---
+++ dashboard/src/components/index.ts
@@ -0,0 +1,14 @@
+export { Alert } from './Alert';
+export { LoadingSpinner } from './LoadingSpinner';
+export { Sidebar } from './Sidebar';
+export { icons } from './icons';
+export { Modal, useModal } from './Modal';
+export { ConfirmDialog, useConfirmDialog } from './ConfirmDialog';
+export {
+  ToastProvider,
+  useToast,
+  useSuccessToast,
+  useErrorToast,
+  useWarningToast,
+  useInfoToast
+} from './Toast';
