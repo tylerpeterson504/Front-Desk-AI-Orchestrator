0de409c20bf06ebb6b09f344d84c0839a2f486c0
tylerpeterson504 <tyler.peterson504@gmail.com>
2026-09-02 09:13:14 -0500

    feat(dashboard): add lib index file for centralized exports

---
+++ dashboard/src/lib/index.ts
@@ -0,0 +1,17 @@
+export {
+  useZodForm,
+  FormSubmitHandler,
+  propertyFormSchema,
+  templateFormSchema,
+  shiftNoteFormSchema,
+  loginFormSchema,
+  registerFormSchema,
+  PropertyFormData,
+  TemplateFormData,
+  ShiftNoteFormData,
+  LoginFormData,
+  RegisterFormData,
+  FormError,
+  FormField,
+  SubmitButton
+} from './formUtils';
